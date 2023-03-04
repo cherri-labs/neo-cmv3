9a3d68ad5b00f0ad552223fda19a859469d3d45a
cherri-labs <cherrynoize9987@outlook.com>
2023-03-04 15:56:36 +0100

    tried removing meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,15 +11,6 @@ const IndexPage = ({}) => {
     <Suspense fallback={`Loading...`}>
       <Head>
         <title>Neo CMv3</title>
-        <meta charSet="utf-8"/>
-        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no"/>
-        <meta name="theme-color" content="#000"/>
-        <meta name="description" content="My very own Candy Machine on Solana"/>
-        <meta name="keywords" content="Neo-CMv3, Candy Machine, Metaplex, Solana, NFT, non-fungible token, Web3, blockchain, SOL, mint, generative art, digital art, pfp, profile picture"/>
-        <meta name="twitter:card" content="summary"/>
-        <meta name="twitter:title" content="Neo-CMv3"/>
-        <meta name="twitter:description" content="My very own Candy Machine on Solana"/>
-        <meta name="twitter:image" content="public/favicon.ico"/>
         <link rel="icon" type="image/x-icon" href="favicon.ico"/>
       </Head>
       <DynamicMain />
--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,6 @@ export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
 export const rpcHost =
   process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
 
-console.log(network, rpcHost);
-
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
     "5huM31dF47yS3JHpjFWgYj3q26iUDqM82oiE6BpCzinU"
